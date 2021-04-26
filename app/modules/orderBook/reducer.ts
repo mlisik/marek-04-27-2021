54132177c2b8944a56330af59f92094b2be92f54
Marek Lisik <marek.lisik@ujet.lu>
2021-04-26 13:09:57 +0200

    defines orderbook state

--- app/modules/orderBook/reducer.ts
+++ app/modules/orderBook/reducer.ts
@@ -1,6 +1,24 @@
 import { createReducer } from 'typesafe-actions';
 
+type Order = [number, number];
+
+export interface OrderBookState {
+  error: any | null;
+  data: {
+    bids: Order[];
+    asks: Order[];
+  };
+}
+
+export const initialState: OrderBookState = {
+  error: null,
+  data: {
+    bids: [],
+    asks: [],
+  },
+};
+
 // TODO: define State and RootAction types
-const reducer = createReducer<any, any>({});
+const reducer = createReducer<OrderBookState, any>(initialState);
 
 export default reducer;
